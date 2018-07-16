613b92e375369b197e37dc54ac5e66fef115c5ac
Valentín Blanco <valentin_gomez1@hotmail.com>
2018-07-16 16:40:58 +0200

    Improvements in recon.py.

--- pinecone/core/utils.py
+++ pinecone/core/utils.py
@@ -1,6 +1,7 @@
 import typing
 
 from pyric import pyw
+from scapy.layers.dot11 import Dot11Elt
 
 
 class IfaceUtils:
@@ -18,3 +19,33 @@ class IfaceUtils:
             pyw.up(interface)
 
         return interface
+
+
+class ScapyUtils:
+    @staticmethod
+    def process_dot11elts(dot11elts: Dot11Elt) -> typing.Dict[str, typing.Any]:
+        dot11elts_info = {
+            "ssid": None,
+            "channel": None,
+            "encryption_types": set(),
+        }
+
+        while isinstance(dot11elts, Dot11Elt):
+            if dot11elts.ID == 0 and dot11elts.len and dot11elts.len > 0:
+                try:
+                    dot11elts_info["ssid"] = dot11elts.info.decode()
+                except:
+                    pass
+            elif dot11elts.ID == 3:
+                try:
+                    dot11elts_info["channel"] = ord(dot11elts.info)
+                except:
+                    pass
+            elif dot11elts.ID == 48:
+                dot11elts_info["encryption_types"].add("WPA2")
+            elif dot11elts.ID == 221 and dot11elts.info.startswith(b"\x00\x50\xf2\x01\x01\x00"):
+                dot11elts_info["encryption_types"].add("WPA")
+
+            dot11elts = dot11elts.payload
+
+        return dot11elts_info
--- recon.py
+++ recon.py
@@ -10,7 +10,7 @@ from pyric import pyw
 from scapy.layers.dot11 import Dot11, Dot11Elt, Packet, Dot11ProbeReq, Dot11Beacon, sniff
 
 from pinecone.core.database import Client, ExtendedServiceSet, ProbeReq, BasicServiceSet
-from pinecone.core.utils import IfaceUtils
+from pinecone.core.utils import IfaceUtils, ScapyUtils
 
 bssids_cache = set()
 clients_cache = set()
@@ -27,18 +27,7 @@ def handle_probe_req(packet: Packet):
     except:
         client = Client(mac=client_mac)
 
-    elt_field = packet[Dot11Elt]
-    ssid = None
-
-    while isinstance(elt_field, Dot11Elt):
-        if elt_field.ID == 0 and elt_field.len and elt_field.len > 0:
-            try:
-                ssid = elt_field.info.decode()
-            except:
-                pass
-
-        elt_field = elt_field.payload
-
+    ssid = ScapyUtils.process_dot11elts(packet[Dot11Elt])["ssid"]
     ess = None
 
     if ssid:
@@ -61,64 +50,49 @@ def handle_probe_req(packet: Packet):
 @db_session
 def handle_beacon(packet: Packet):
     now = datetime.now()
-    channel = None
-    elt_field = packet[Dot11Elt]
-    encryption_types = set()
-    ssid = None
 
-    while isinstance(elt_field, Dot11Elt):
-        if elt_field.ID == 0 and elt_field.len and elt_field.len > 0:
-            try:
-                ssid = elt_field.info.decode()
-            except:
-                pass
-        elif elt_field.ID == 3:
-            try:
-                channel = ord(elt_field.info)
-            except:
-                pass
-        elif elt_field.ID == 48:
-            encryption_types.add("WPA2")
-        elif elt_field.ID == 221 and elt_field.info.startswith(b"\x00\x50\xf2\x01\x01\x00"):
-            encryption_types.add("WPA")
-
-        elt_field = elt_field.payload
+    dot11elts_info = ScapyUtils.process_dot11elts(packet[Dot11Elt])
+    ssid = dot11elts_info["ssid"]
+    channel = dot11elts_info["channel"]
+    encryption_types = ", ".join(dot11elts_info["encryption_types"])
+    hides_ssid = not ssid
+    ess = None
+    bssid = packet[Dot11].addr3
 
     if channel is None:
         channel = iface_current_channel
 
     if not encryption_types:
-        capabilities = packet.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}").split('+')
-        encryption_types.add("WEP" if "privacy" in capabilities else "OPN")
-
-    encryption_type = encryption_types.pop()
-    ess = None
-    hides_ssid = False
+        capabilities = packet.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}")
+        encryption_types = "WEP" if "privacy" in capabilities else "OPN"
 
     if ssid:
         try:
             ess = ExtendedServiceSet[ssid]
         except:
             ess = ExtendedServiceSet(ssid=ssid)
-    else:
-        hides_ssid = True
-
-    bssid = packet[Dot11].addr3
 
     try:
         bss = BasicServiceSet[bssid]
         bss.channel = channel
-        bss.encryption = encryption_type
+        bss.encryption_types = encryption_types
+        bss.cipher_types = ""
+        bss.authn_types = ""
         bss.last_seen = now
         bss.ess = ess
         bss.hides_ssid = hides_ssid
     except:
-        bss = BasicServiceSet(bssid=bssid, channel=channel, encryption=encryption_type, last_seen=now,
-                              ess=ess, hides_ssid=hides_ssid)
+        BasicServiceSet(bssid=bssid, channel=channel, encryption_types=encryption_types, cipher_types="",
+                        authn_types="", last_seen=now, ess=ess, hides_ssid=hides_ssid)
 
     if bssid not in bssids_cache:
         bssids_cache.add(bssid)
-        print("[i] Detected AP: ({}).".format(bss))
+
+        ssid = "\"{}\"".format(ssid) if ssid else "<empty>"
+        print("[i] Detected AP: SSID: {}, BSSID: {}, ch: {}, enc: ({}), cipher: ({}), authn: ({}).".format(ssid, bssid,
+                                                                                                           channel,
+                                                                                                           encryption_types,
+                                                                                                           "", ""))
 
 
 def handle_packet(packet: Packet):
