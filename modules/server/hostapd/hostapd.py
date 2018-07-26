ce3ac66e8f468e8f0d4512870bedff1cdd243656
Valentín Blanco <valentin_gomez1@hotmail.com>
2018-07-26 16:03:42 +0200

    Added hostapd module.

---
+++ modules/server/hostapd/hostapd.py
@@ -0,0 +1,40 @@
+import argparse
+from subprocess import run
+
+from pathlib2 import Path
+
+from pinecone.core.module import DaemonBaseModule
+from pinecone.core.main import Pinecone
+
+
+class Module(DaemonBaseModule):
+    META = {
+        "id": "modules/server/hostapd",
+        "name": "",
+        "author": "",
+        "version": "",
+        "description": "",
+        "options": argparse.ArgumentParser()
+    }
+    META["options"].add_argument("-i", "--iface", help="wlan interface", default="wlan0", type=str)
+    META["options"].add_argument("-c", "--channel", default=1, type=int)
+    META["options"].add_argument("-e", "--encryption", default="WPA2", type=str)
+    META["options"].add_argument("-p", "--password", default="password12345", type=str)
+    META["options"].add_argument("-s", "--ssid", default="PINECONEWIFI", type=str)
+
+    PROCESS_NAME = "hostapd"
+    CONFIG_TEMPLATE_PATH = Path(Path(__file__).parent, "hostapd_template.conf")
+    CONFIG_FILENAME = "hostapd.conf"
+
+    def __init__(self):
+        super().__init__()
+
+    def _launch(self) -> int:
+        return run([self.PROCESS_NAME, "-B", str(self.config_path)]).returncode
+
+    def run(self, args: argparse.Namespace, cmd: Pinecone) -> None:
+        for wpaSupplicantProc in self.search_procs("wpa_supplicant"):
+            if any(args.iface in cmdLine for cmdLine in wpaSupplicantProc.cmdline()):
+                wpaSupplicantProc.terminate()
+
+        super().run(args, cmd)
--- pinecone/core/module.py
+++ pinecone/core/module.py
@@ -1,5 +1,11 @@
 from abc import ABC, abstractmethod
 import argparse
+import sys
+from typing import Generator
+
+from pathlib2 import Path
+from jinja2 import Template
+from psutil import process_iter, Process
 
 from pinecone.core.main import Pinecone
 
@@ -21,3 +27,52 @@ class BaseModule(ABC):
     @abstractmethod
     def stop(self, cmd: Pinecone) -> None:
         pass
+
+class DaemonBaseModule(BaseModule):
+    TMP_FOLDER_PATH = Path(sys.path[0], "tmp").resolve()
+
+    PROCESS_NAME = None
+    CONFIG_TEMPLATE_PATH = None
+    CONFIG_FILENAME = None
+
+    @abstractmethod
+    def __init__(self):
+        self.process = None
+        self.config_path = Path(self.TMP_FOLDER_PATH, self.CONFIG_FILENAME)
+
+    def is_running(self) -> bool:
+        return self.process is not None and self.process.is_running()
+
+    def stop(self, cmd: Pinecone) -> None:
+        if self.is_running():
+            self.process.terminate()
+            self.process = None
+
+    @abstractmethod
+    def _launch(self) -> int:
+        pass
+
+    def run(self, args: argparse.Namespace, cmd: Pinecone) -> None:
+        self._term_same_procs()
+
+        config_template = Template(self.CONFIG_TEMPLATE_PATH.read_text())
+        self.TMP_FOLDER_PATH.mkdir(exist_ok=True)
+        self.config_path.write_text(config_template.render(vars(args)))
+
+        if self._launch() == 0:
+            self.process = next(self._search_same_procs(), None)
+
+    @staticmethod
+    def search_procs(process_name) -> Generator[Process, None, None]:
+        for p in process_iter(attrs=["name"]):
+            if p.info["name"] == process_name:
+                yield p
+
+    @classmethod
+    def _search_same_procs(cls) -> Generator[Process, None, None]:
+        return cls.search_procs(cls.PROCESS_NAME)
+
+    @classmethod
+    def _term_same_procs(cls) -> None:
+        for p in cls._search_same_procs():
+            p.terminate()
