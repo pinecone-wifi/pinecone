e3db71a9d98e6a4caf60c9b85afa51bcd26ffa81
Valentín Blanco <valentin_gomez1@hotmail.com>
2020-11-07 17:17:36 +0100

    Update install.sh script due to py2neo dependency.

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ apt install -y python3-venv dnsmasq hostapd-wpe
 rm -rf venv
 python3 -m venv venv
 . venv/bin/activate
-pip3 install --upgrade pip setuptools wheel
+# Using pip 20.2.* with the old resolver until py2neo updates its dependency of prompt_toolkit to version >=3...
+pip3 install --upgrade 'pip==20.2.*' setuptools wheel
 pip3 install -r requirements.txt
+echo
+echo "Installation of py2neo package will throw an error until its dependency of prompt_toolkit is updated to version >=3, but it should work fine for Pinecone use case."
