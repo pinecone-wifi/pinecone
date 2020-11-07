8f95ba136ca504085ae44f79131f29611799b16d
Valentín Blanco <valentin_gomez1@hotmail.com>
2020-11-07 13:09:13 +0100

    Updated the install.sh script.

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# This installation script currently installs Kali Linux repo packages.
+apt update
+apt install -y python3-venv dnsmasq hostapd-wpe
 rm -rf venv
 python3 -m venv venv
 . venv/bin/activate
